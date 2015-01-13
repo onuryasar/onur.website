8040d410117c6a5fc5919ced1e23a887f97839ac
Onur Yasar <onur@Onur-desiBel-iMac.local>
2015-01-13 23:33:56 +0200

    ilk yükleme

---
+++ public/index.php
@@ -0,0 +1,84 @@
+<?php
+/*
+if($_SERVER['SERVER_NAME']!="onur.website"){
+	header("HTTP/1.1 301 Moved Permanently");
+	header("Location: http://onur.website/");
+	die();
+}
+*/
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="tr-TR">
+
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<title>onur yaşar</title>
+
+<link rel="stylesheet" href="http://onur.website/blog/wp-content/themes/clean-home/reset.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="http://onur.website/blog/wp-content/themes/clean-home/intro.css" type="text/css" media="screen" />
+<!--[if IE]><link rel="stylesheet" href="http://onur.website/blog/wp-content/themes/clean-home/ie.css" type="text/css" media="screen" /><![endif]-->
+
+	<style type="text/css">
+		#fancybox-close{right:-15px;top:-15px}
+		div#fancybox-content{border-color:#FFFFFF}
+		div#fancybox-title{background-color:#FFFFFF}
+		div#fancybox-outer{background-color:#FFFFFF}
+		div#fancybox-title-inside{color:#333333}
+		.intro h2.twitter{
+		    background: url(img/icons/twitter.png) no-repeat right center;
+		}
+		.intro h2.linkedin{
+		    background: url(img/icons/linkedin.png) no-repeat right center;
+		}
+		.intro h2.wordpress{
+		    background: url(img/icons/wordpress.png) no-repeat right center;
+		}
+		.intro h2.lastfm{
+		    background: url(img/icons/lastfm.png) no-repeat right center;
+		}
+		.intro h2.instagram{
+		    background: url(img/icons/instagram.png) no-repeat right center;
+		}
+		.intro h2.flickr{
+		    background: url(img/icons/flickr.png) no-repeat right center;
+		}
+		.intro h2.letterboxd{
+		    background: url(img/icons/letterboxd.png) no-repeat right center;
+		}
+	</style>
+</head>
+
+<body>
+
+<div id="wrapper" style="width:360px;">
+
+	<div id="header" style="margin-top:60px; width:360px; border-bottom:none;">
+		<div id="logo" class="intro">
+			<h1><a>onur yaşar</a></h1>
+			<div style="text-align:right; margin-top:40px;">
+				<p class="onur"><img src="img/onur.jpg"></p>
+				<h2 class="twitter"><a href="http://twitter.com/onuryasar" target="_blank">twitter</a></h2>
+				<h2 class="twitter"><a href="http://twitter.com/onuryasar_en" target="_blank">twitter (english)</a></h2>
+				<h2 class="linkedin"><a href="http://www.linkedin.com/profile/view?id=15245820" target="_blank">linked in</a></h2>
+				<h2 class="instagram"><a href="http://www.instagram.com/onuryasar" target="_blank">instagram</a></h2>
+				<h2 class="lastfm"><a href="http://www.last.fm/user/onuryasar" target="_blank">last.fm</a></h2>
+				<h2 class="letterboxd"><a href="http://letterboxd.com/onuryasar/" target="_blank">letterboxd</a></h2>
+				<h2 class="wordpress"><a href="http://onur.website/blog/">türkçe blog</a></h2>
+				<h2 class="wordpress"><a href="http://onur.website/blog-en/">english blog</a></h2>
+			</div>
+		</div>
+	</div>
+
+</div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-57053142-1', 'auto');
+  ga('send', 'pageview');
+</script>
+</body>
+</html>
